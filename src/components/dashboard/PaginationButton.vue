4832f032e447ec46fd9ddfe9004eef8ba92bde75
arif.pepaj <aripepaj03@gmail.com>
2024-10-07 16:38:46 +0200

    Add paggination

---
+++ src/components/dashboard/PaginationButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <button
+      :class="[ 
+        'px-3 py-1 rounded font-sans font-normal text-small border border-gray-300', 
+        isActive ? 'bg-red-600 text-white' : 'bg-white text-gray-700',
+        'hover:bg-gray-200 active:bg-gray-300'
+      ]"
+      @click="handleClick"
+    >
+      {{ label }}
+    </button>
+  </template>
+  
+  <script setup lang="ts">
+  const props = defineProps<{
+    label: string;
+    isActive: boolean;
+    page: number;
+  }>();
+  
+  const emit = defineEmits(['update:page']);
+  
+  const handleClick = () => {
+    emit('update:page', props.page);
+  };
+  </script>  
